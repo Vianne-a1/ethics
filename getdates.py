ad60e696e77de708a62430e2c7e089f056d73329
Tiffany Yang <121731114+Vianne-a1@users.noreply.github.com>
2025-04-10 21:32:25 +0000

    a

--- getdates.py
+++ getdates.py
@@ -1,19 +1,23 @@
 import re
 
 def find_dates(file_path):
-    date_pattern = r'(/((\d{1,2}(/|-)){2}\d{2,4})|(\d{1,2} ((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)) \d{4})|(((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)).* \d{1,2},* \d{4})|(((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)),* \d{4})|(\d{1,2}/\d{4})|([12][0-9]{3})|((19|20)\d{2})/gm)'
+    date_pattern = r'((\d{1,2}[/-]\d{1,2}[/-]\d{2,4})|(\d{1,2} (?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) \d{4})|((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?).* \d{1,2},* \d{4})|((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?),* \d{4})|(\d{1,2}/\d{4})|(?<!\d{3} )([12][0-9]{3})|(\d{1,2}/\d{4}))'
 
     try:
         with open(file_path, 'r') as file:
-            content = file.read()
-            matches = re.findall(date_pattern, content)
-            dates = [match for group in matches for match in group if match]
-            counter = len(dates)
-            if dates:
-                print(f"Dates found in the file ({counter} total):")
-                for date in dates:
-                    print(date)
-                print(f"\nTotal dates found: {counter}")
+            total_unique_dates = set()
+            print("Dates found in the file:")
+            for line_num, line in enumerate(file, start=1):
+                matches = re.findall(date_pattern, line)
+                unique_line_dates = {match for group in matches for match in group if match}  
+                if unique_line_dates:
+                    for date in unique_line_dates:
+                        print(f"Line {line_num}: {date}")
+                    total_unique_dates.update(unique_line_dates)  
+            
+            total_count = len(total_unique_dates)
+            if total_count > 0:
+                print(f"\nTotal unique dates found: {total_count}")
             else:
                 print("No dates found in the file.")
     except FileNotFoundError:
